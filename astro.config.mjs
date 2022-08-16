6a48696184835a89207953c5ffdb202b75137e17
Greg Hunt <greg@motto.ca>
2022-08-16 18:13:28 -0400

    updated packages and i18

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,20 @@ import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), astroI18next({
+  integrations: [preact(), tailwind(),
+  astroI18next({
     baseLanguage: "en",
     i18next: {
       debug: true,
       // convenient during development to check for missing keys
       supportedLngs: ["en", "fr"]
     }
-  }), image(), mdx()],
+  }),
+  image(), mdx()],
   vite: {
     ssr: {
       external: ["svgo"]
