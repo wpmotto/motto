0b26c0b2b518d42a3c16b80b69ebcff537e309d8
Greg Hunt <greg@motto.ca>
2022-08-16 18:18:46 -0400

    Removed i18 pacakge ref

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
-import astroI18next from "astro-i18next";
+// import astroI18next from "astro-i18next";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind(),
-  astroI18next({
-    baseLanguage: "en",
-    i18next: {
-      debug: true,
-      // convenient during development to check for missing keys
-      supportedLngs: ["en", "fr"]
-    }
-  }),
+  // astroI18next({
+  //   baseLanguage: "en",
+  //   i18next: {
+  //     debug: true,
+  //     // convenient during development to check for missing keys
+  //     supportedLngs: ["en", "fr"]
+  //   }
+  // }),
   image(), mdx()],
   vite: {
     ssr: {
