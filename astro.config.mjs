760824742613f6751da4d9d83fb3567f19e30bd7
Greg Hunt <greg@motto.ca>
2022-08-17 16:33:07 -0400

    Got i18next working

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,38 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
+import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: "https://motto.ca/",
   experimental: {
     integrations: true,
   },
   integrations: [
     preact(),
     tailwind(),
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          fr: "fr",
+        },
+      },
+    }),
     astroI18next({
       baseLocale: "en",
+      supportedLocales: ["en", "fr"],
       i18next: {
-        debug: true, // convenient during development to check for missing keys
-        supportedLngs: ["en", "fr"],
+        debug: true,
+        initImmediate: false,
+        backend: {
+          loadPath: "./src/locales/{{lng}}.json",
+        },
       },
+      i18nextPlugins: { fsBackend: "i18next-fs-backend" },
     }),
   ],
   vite: {
