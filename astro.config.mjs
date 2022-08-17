1de70a0bb9624f0c7d84ff1d6158f91aa0cdeef1
Greg Hunt <greg@motto.ca>
2022-08-17 11:19:47 -0400

    Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare';
 // import astroI18next from "astro-i18next";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [preact(), tailwind(),
   // astroI18next({
   //   baseLanguage: "en",
