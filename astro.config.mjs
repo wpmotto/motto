c817bc467859f4b5f09f76b4a9970ff744fc369b
Greg Hunt <greg@motto.ca>
2022-08-17 12:21:03 -0400

    Use our own package until support for 1.0.1 arrives

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
-import cloudflare from '@astrojs/cloudflare';
-// import astroI18next from "astro-i18next";
-import image from "@astrojs/image";
+import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
-  integrations: [preact(), tailwind(),
-  // astroI18next({
-  //   baseLanguage: "en",
-  //   i18next: {
-  //     debug: true,
-  //     // convenient during development to check for missing keys
-  //     supportedLngs: ["en", "fr"]
-  //   }
-  // }),
-  image(), mdx()],
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    preact(),
+    tailwind(),
+    astroI18next({
+      baseLocale: "en",
+      i18next: {
+        debug: true, // convenient during development to check for missing keys
+        supportedLngs: ["en", "fr"],
+      },
+    }),
+    mdx()
+  ],
   vite: {
     ssr: {
       external: ["svgo"]
